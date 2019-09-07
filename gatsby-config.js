e4d802db3e3561b9e37c31529297f8949ca5b85c
Ryan Boone <falldowngoboone@gmail.com>
2019-09-07 13:11:32 -0500

    Fresh start

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,114 +1,24 @@
 module.exports = {
-  siteMetadata: {
-    title: `fall down go boone`,
-    author: `Ryan Boone`,
-    description: `The personal blog of a developer at The Container Store.`,
-    siteUrl: `https://www.falldowngoboone.com`,
-    social: {
-      twitter: `therealboone`,
-    },
-  },
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
+      resolve: `gatsby-theme-blog`,
+      options: {},
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ 'content:encoded': edge.node.html }],
-                });
-              });
-            },
-            query: `
-              {
-                allMarkdownRemark(
-                  sort: { order: DESC, fields: [frontmatter___date] },
-                ) {
-                  edges {
-                    node {
-                      excerpt
-                      html
-                      fields { slug }
-                      frontmatter {
-                        title
-                        date
-                      }
-                    }
-                  }
-                }
-              }
-            `,
-            output: '/rss.xml',
-            title: 'fall down go boone',
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-emotion`,
   ],
-};
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `falldowngoboone`,
+    author: `Ryan Boone`,
+    description: `Texas-based designer and developer`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/therealboone`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/falldowngoboone`,
+      },
+    ],
+  },
+}
--- gatsby-ssr.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { ThemeProvider } from './src/context/theme';
-
-export const wrapRootElement = ({ element }) => (
-  <ThemeProvider>{element}</ThemeProvider>
-);
