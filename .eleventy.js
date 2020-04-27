6c8d9056a5b967af8f8e9eb51a87d2587792982c
Ryan Boone <falldowngoboone@gmail.com>
2020-04-26 20:58:58 -0500

    Style adjustments
    Including some fonts.

--- .eleventy.js
+++ .eleventy.js
@@ -87,6 +87,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection('tagList', require('./_11ty/getTagList'));
 
   eleventyConfig.addPassthroughCopy('./src/img');
+  eleventyConfig.addPassthroughCopy('./src/fonts');
   eleventyConfig.addWatchTarget('./src/sass');
 
   /* Markdown Overrides */
@@ -119,23 +120,12 @@ module.exports = function (eleventyConfig) {
   });
 
   return {
-    templateFormats: ['md', 'njk', 'html', 'liquid'],
-
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about those.
-
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for link URLs (it does not affect your file structure)
-    // Best paired with the `url` filter: https://www.11ty.io/docs/filters/url/
-
-    // You can also pass this in on the command line using `--pathprefix`
-    // pathPrefix: "/",
+    templateFormats: ['md', 'html', 'liquid'],
 
     markdownTemplateEngine: 'liquid',
     htmlTemplateEngine: 'liquid',
     dataTemplateEngine: 'liquid',
 
-    // These are all optional, defaults are shown:
     dir: {
       input: './src',
       includes: '_includes',
