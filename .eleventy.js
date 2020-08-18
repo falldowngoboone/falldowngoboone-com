429acae52bf4b6e6b5232084a7eb3def687f9cef
Ryan Boone <falldowngoboone@gmail.com>
2020-08-18 18:37:34 -0500

    Initial eleventy
    I'm adding Sass here, some initial markup, and I'm moving the blog
    posts. I may to move the blog posts again depending on the limitations
    of the eleventy configuration.

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function (eleventyConfig) {
+  // handle the CSS injection in BrowserSync correctly
+  // when watching Sass, we output to the `_site` directory
+  eleventyConfig.setBrowserSyncConfig({
+    files: '_site/**/*.css',
+  });
+
+  return {
+    dir: {
+      input: 'src',
+    },
+  };
+};
