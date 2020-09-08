4f7f6e835440da3dfc87aebd65efda8e998e5b2a
Ryan Boone <falldowngoboone@gmail.com>
2020-09-07 19:50:35 -0500

    Add syntax highlighting
    Using Prism with Oceanic Next, punched up with a filter. I may try and
    move the override into a custom theme, but this works for now.
    
    I had to add a utility style thanks to `pre`'s unformatted whitespace,
    which causes grid columns, which have a min of `auto` to not collapse
    like I want them to. The fix is `min-width: 0`, which is easily done via
    a utility class.
    
    I am also adding a passthrough for JS. We'll eventually tool up the JS
    workflow as needed.

--- .eleventy.js
+++ .eleventy.js
@@ -28,6 +28,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('src/fonts');
   eleventyConfig.addPassthroughCopy('src/images');
+  eleventyConfig.addPassthroughCopy('src/js');
 
   eleventyConfig.addCollection('tagList', function (collection) {
     let tagSet = new Set();
