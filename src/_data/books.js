7f54e13721edc0c26d241c6df08e910eae1ba376
Ryan Boone <falldowngoboone@gmail.com>
2020-10-14 20:06:44 -0500

    Add book partial

--- src/_data/books.js
+++ src/_data/books.js
@@ -12,7 +12,7 @@ const instanceBooks = new WeakMap();
  *   author: string;
  *   img?: string;
  *   url: string;
- *   rating?: 1|1.5|2|2.5|3|3.5|4|4.5|5;
+ *   rating?: 0|1|1.5|2|2.5|3|3.5|4|4.5|5;
  *   notes?: string;
  *   started?: Date|string;
  *   completed?: Date|string;
